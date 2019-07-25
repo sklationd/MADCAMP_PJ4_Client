378c7726e6ecea9af0c73ac1f8b16a0a73475c73
김선규 <sklationd@gmail.com>
2019-07-25 16:07:30 +0900

    search

--- lib/friends.dart
+++ lib/friends.dart
@@ -19,7 +19,8 @@ class SearchUsers extends StatefulWidget {
   SearchUsersState createState() => new SearchUsersState();
 }
 
-class SearchUsersState extends State<SearchUsers> with AutomaticKeepAliveClientMixin {
+class SearchUsersState extends State<SearchUsers>
+    with AutomaticKeepAliveClientMixin {
   GetUsers gu = new GetUsers();
   List<User> _userList = List<User>();
   List<User> _userListForDisplay = List<User>();
@@ -45,8 +46,10 @@ class SearchUsersState extends State<SearchUsers> with AutomaticKeepAliveClientM
     return new Scaffold(
       resizeToAvoidBottomPadding: false,
       appBar: new AppBar(
-        title: new Text("멋진 유져들"),
-        backgroundColor: Colors.lightBlueAccent,
+        title: new Text(
+          "Friends",
+        ),
+        backgroundColor: Colors.white,
         centerTitle: true,
       ),
       body: _userListBuilder(),
@@ -62,23 +65,18 @@ class SearchUsersState extends State<SearchUsers> with AutomaticKeepAliveClientM
             return _searchBar();
           } else {
             return new InkWell(
-              onTap: () => _showUser(context, index - 1),
-              child: _listUser(index - 1)
-            );
+                onTap: () => _showUser(context, index - 1),
+                child: _listUser(index - 1));
           }
 //          return index == 0 ? _searchBar() : _listUser(context, index - 1);
         },
       );
     } else {
       return Center(
-          child: new Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                new CircularProgressIndicator(),
-                new Text("Loading all users of this app!")
-              ]
-          )
-      );
+          child: new Row(mainAxisSize: MainAxisSize.min, children: [
+        new CircularProgressIndicator(),
+        new Text("Loading all users of this app!")
+      ]));
     }
   }
 
@@ -87,134 +85,114 @@ class SearchUsersState extends State<SearchUsers> with AutomaticKeepAliveClientM
         padding: const EdgeInsets.all(8.0),
         child: TextField(
           decoration: InputDecoration(
-              hintText: 'Search users..',
-              prefixIcon: Icon(Icons.search),
+            hintText: 'Search users..',
+            prefixIcon: Icon(Icons.search),
           ),
           onChanged: (text) {
             text = text.toLowerCase();
             List<User> userList = _userList.where((user) {
-              var userName = user.getFirstname().toLowerCase();
+              var userName = user.getFirstname().toLowerCase() +
+                  ' ' +
+                  user.getLastname().toLowerCase();
               return userName.contains(text);
             }).toList();
             setState(() {
               _userListForDisplay = userList;
             });
           },
-        )
-    );
+        ));
   }
 
   _listUser(index) {
     return Card(
         child: Padding(
-            padding: const EdgeInsets.only(
-                top: 20.0, bottom: 20.0, left: 16.0, right: 16.0),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    _userListForDisplay[index].getFirstname() + " " +
-                        _userListForDisplay[index].getLastname(),
-                    style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  Text(
-                    _userListForDisplay[index].getUsername(),
-                    style: TextStyle(
-                        color: Colors.grey.shade600
-                    ),
-                  ),
-                ]
+      padding: const EdgeInsets.only(
+          top: 20.0, bottom: 20.0, left: 16.0, right: 16.0),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              _userListForDisplay[index].getFirstname() +
+                  " " +
+                  _userListForDisplay[index].getLastname(),
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
-        )
-    );
+            Text(
+              _userListForDisplay[index].getUsername(),
+              style: TextStyle(color: Colors.grey.shade600),
+            ),
+          ]),
+    ));
   }
 
   void _showUser(context, index) {
     showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (BuildContext context) {
-        return Dialog(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-          child: Container(
-            height: 350.0,
-            width: 200.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20.0)
-            ),
-            child: Column(
-              children: <Widget>[
-                Stack(
-                  children: <Widget>[
-                    Container(
-                      height: 170.0
-                    ),
-                    Container(
-                      height: 100.0,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(10.0),
-                          topRight: Radius.circular(10.0),
-                        ),
-                        color: Colors.lightBlueAccent
-                      ),
-                    ),
-                    Positioned(
-                      top: 40.0,
-                      left: 80.0,
-                      child: Container(
-                        height: 120.0,
-                        width: 120.0,
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return Dialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: Container(
+                  height: 350.0,
+                  width: 200.0,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(20.0)),
+                  child: Column(children: <Widget>[
+                    Stack(children: <Widget>[
+                      Container(height: 170.0),
+                      Container(
+                        height: 100.0,
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(60.0),
-                          border: Border.all(
-                            color: Colors.white,
-                            style: BorderStyle.solid,
-                            width: 2.0
-                          ),
-                          image: DecorationImage(
-                            image: NetworkImage('http://i.pravatar.cc/300'),
-                            fit: BoxFit.cover
-                          )
-                        ),
-                      )
-                    )
-                  ]
-                ),
-                SizedBox(height: 10.0),
-                Padding(
-                  padding: EdgeInsets.all(3.0),
-                  child: Text(
-                    _userListForDisplay[index].getFirstname() + " " +
-                          _userListForDisplay[index].getLastname(),
-                    style: TextStyle(
-                      fontFamily: 'Quicksand',
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.bold,
-                    )
-                  )
-                ),
-                SizedBox(height: 4.0),
-                FlatButton(
-                  child: Center(
-                    child: Text(
-                        _userListForDisplay[index].getEmail(),
-                      style: TextStyle(
-                        fontFamily: 'Quicksand',
-                        fontSize: 12.0,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10.0),
+                              topRight: Radius.circular(10.0),
+                            ),
+                            color: Colors.lightBlueAccent),
                       ),
-                    ),
-                  ),
-                  onPressed: () {}
-                )
-              ]
-            )
-          )
-        );
-      }
-    );
+                      Positioned(
+                          top: 40.0,
+                          left: 80.0,
+                          child: Container(
+                            height: 120.0,
+                            width: 120.0,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(60.0),
+                                border: Border.all(
+                                    color: Colors.white,
+                                    style: BorderStyle.solid,
+                                    width: 2.0),
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                        'http://i.pravatar.cc/300'),
+                                    fit: BoxFit.cover)),
+                          ))
+                    ]),
+                    SizedBox(height: 10.0),
+                    Padding(
+                        padding: EdgeInsets.all(3.0),
+                        child: Text(
+                            _userListForDisplay[index].getFirstname() +
+                                " " +
+                                _userListForDisplay[index].getLastname(),
+                            style: TextStyle(
+                              fontFamily: 'Quicksand',
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                            ))),
+                    SizedBox(height: 4.0),
+                    FlatButton(
+                        child: Center(
+                          child: Text(
+                            _userListForDisplay[index].getEmail(),
+                            style: TextStyle(
+                              fontFamily: 'Quicksand',
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ),
+                        onPressed: () {})
+                  ])));
+        });
   }
 }
